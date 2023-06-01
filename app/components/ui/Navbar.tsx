d712bd41284a5f4c7bd2c1bb83658a0b5d06de87
Teo Fumagalli <teo.fumagalli@aerolab.co>
2023-06-01 20:03:52 -0300

    feat: basic navigation

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,12 +1,62 @@
+import { DashboardIcon, FilePlusIcon } from '@radix-ui/react-icons';
+import { LinkButton } from './Button';
+import cn from 'classnames';
+import type { RouteMatch } from '@remix-run/react';
+import { useMatches } from '@remix-run/react';
+
 type NavbarProps = React.PropsWithChildren<{}>;
 
+interface NavbarLinkProps {
+  currentRoute: RouteMatch;
+  route: string;
+  text: string;
+  icon: React.ReactNode;
+}
+
+function NavbarLink({ currentRoute, route, text, icon }: NavbarLinkProps) {
+  return (
+    <LinkButton
+      to={route}
+      className={cn('py-2 px-4 justify-start rounded-lg transition', {
+        'bg-green-100': currentRoute.pathname === route,
+      })}
+    >
+      {icon}
+      <span>{text}</span>
+    </LinkButton>
+  );
+}
+
 export default function Navbar({ children }: NavbarProps) {
+  const [, , child] = useMatches();
+
   return (
     <div className="flex flex-col h-full">
       <div className="font-display text-4xl text-center">
         Cook
         <span className="text-green-700">Book</span>
       </div>
+
+      <nav>
+        <ul className="mt-6 flex flex-col gap-1">
+          <li>
+            <NavbarLink
+              currentRoute={child}
+              route="/recipes"
+              text="My Recipes"
+              icon={<DashboardIcon />}
+            />
+          </li>
+          <li>
+            <NavbarLink
+              currentRoute={child}
+              route="/recipes/new"
+              text="New Recipe"
+              icon={<FilePlusIcon />}
+            />
+          </li>
+        </ul>
+      </nav>
 
       <div className="mt-auto pt-6 border-t border-black border-opacity-10">
         {children}
