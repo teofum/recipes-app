2c080e9982e4c917d9df2a9df6925c5b0e14717e
Teo Fumagalli <teo.fumagalli@aerolab.co>
2023-06-02 02:26:48 -0300

    chore: made config changed for vercel deployment

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   tailwind: true,
-  ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ['**/.*'],
+  // When running locally in development mode, we use the built-in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
+  serverBuildPath: 'api/index.js',
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
