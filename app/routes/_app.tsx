0aa666f32a1b528aefe93cff04447b73ef4546ad
Teo Fumagalli <teo.fumagalli@aerolab.co>
2023-06-09 11:37:25 -0300

    feat: mobile layout

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,16 +1,17 @@
+import { useState } from 'react';
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import type { RouteMatch } from '@remix-run/react';
 import { useRouteError } from '@remix-run/react';
-import { Form, Outlet, useLoaderData, useMatches } from '@remix-run/react';
+import { Outlet, useLoaderData } from '@remix-run/react';
 import cn from 'classnames';
-import { useState } from 'react';
-import RouteError from '~/components/RouteError';
+
 import { SidenavIcon } from '~/components/icons';
-import Avatar from '~/components/ui/Avatar';
-import Button, { LinkButton } from '~/components/ui/Button';
-import Navbar from '~/components/ui/Navbar';
+import RouteError from '~/components/RouteError';
+import Button from '~/components/ui/Button';
+import Sidenav from '~/components/ui/Sidenav';
+
 import { getUser } from '~/server/session.server';
+import MobileNavbar from '~/components/ui/MobileNavbar';
 
 export async function loader({ request }: LoaderArgs) {
   const user = await getUser(request);
@@ -20,18 +21,25 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function AppRoute() {
   const { user } = useLoaderData<typeof loader>();
-  const matches = useMatches();
-  const currentRoute = matches.at(-1) as RouteMatch;
 
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className="h-screen grid grid-cols-1 xl:grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
+    <div
+      className="
+        h-screen grid grid-cols-1 grid-rows-[1fr_auto]
+        sm:grid-rows-[auto_1fr]
+        xl:grid-cols-[auto_1fr]
+      "
+    >
+      {/* Header, shown on sm+ */}
       <header
         className="
-          flex flex-row items-center p-2 h-14
+          hidden sm:flex
+          flex-row items-center p-2 h-14
           bg-white border-b border-black border-opacity-10
-          row-start-1 row-span-1 col-start-1 xl:col-[1/span_2]
+          row-start-1 row-span-1 col-start-1
+          xl:col-[1/span_2]
         "
       >
         <div className="flex flex-row gap-2 w-60 items-center z-20">
@@ -52,9 +60,12 @@ export default function AppRoute() {
         </div>
       </header>
 
+      {/* Sidenav, shown on sm+, collapsible until xl+  */}
       <div
         className={cn(
-          'col-start-1 col-span-1 row-start-1 row-span-2 h-full overflow-hidden transition-all',
+          'hidden sm:block',
+          'col-start-1 col-span-1 row-start-1 row-span-2 h-full z-10',
+          'overflow-hidden transition-all',
           {
             'w-0 xl:w-64': !expanded,
             'w-64': expanded,
@@ -63,59 +74,28 @@ export default function AppRoute() {
       >
         <aside
           className="
-          p-4 border-r border-black border-opacity-10
-          bg-white bg-opacity-70 backdrop-blur-lg h-full w-64
-          z-10 relative
-        "
+            p-4 border-r border-black border-opacity-10
+            bg-white bg-opacity-70 backdrop-blur-lg h-full w-64
+          "
         >
-          <Navbar>
-            {user !== null ? (
-              <div className="flex flex-col gap-2">
-                <div className="flex flex-row gap-2 items-center">
-                  <Avatar alt={user.displayName} />
-                  <div className="flex-1">
-                    <div className="font-semibold leading-5">
-                      {user.displayName}
-                    </div>
-                    <div className="text-xs text-stone-500 leading-5">
-                      @{user.username}
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-row gap-1">
-                  <LinkButton
-                    to={`/account`}
-                    variant={{ size: 'sm' }}
-                    className="flex-1"
-                  >
-                    Account
-                  </LinkButton>
-                  <Form
-                    method="post"
-                    action={`/logout?redirectUrl=${currentRoute.pathname}`}
-                    className="flex-1"
-                  >
-                    <Button
-                      type="submit"
-                      variant={{ size: 'sm' }}
-                      className="w-full"
-                    >
-                      Logout
-                    </Button>
-                  </Form>
-                </div>
-              </div>
-            ) : (
-              <LinkButton to={`/login?redirectUrl=${currentRoute.pathname}`}>
-                Sign in
-              </LinkButton>
-            )}
-          </Navbar>
+          <Sidenav user={user} />
         </aside>
       </div>
 
-      <div className="overflow-y-auto row-start-2 row-span-1 col-start-1 xl:col-start-2">
+      {/* Main content */}
+      <div
+        className="
+          overflow-y-auto row-span-1 row-start-1 col-start-1
+          sm:row-start-2
+          xl:col-start-2
+        "
+      >
         <Outlet />
+      </div>
+
+      {/* Mobile navbar, hidden on sm+ */}
+      <div className="h-16 sm:hidden bg-white border-t border-black border-opacity-10">
+        <MobileNavbar />
       </div>
     </div>
   );
