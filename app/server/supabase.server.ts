13891bab82f970a1f9662eb40f7b2a9e24c455f0
Teo Fumagalli <teo.fumagalli@aerolab.co>
2023-06-15 15:50:32 -0300

    hotfix: fixed hardcoded dev supabase url

--- app/server/supabase.server.ts
+++ app/server/supabase.server.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
+const host = process.env.SUPABASE_HOST;
+if(!host) throw new Error('SUPABASE_HOST envvar missing');
+
 const apiKey = process.env.SUPABASE_API_KEY;
 if (!apiKey) throw new Error('SUPABASE_API_KEY envvar missing');
 
-const supabase = createClient(
-  'https://tnfykxgkmfigvbcikbeb.supabase.co',
-  apiKey,
-);
+const supabase = createClient(host, apiKey);
 
 export default supabase;
